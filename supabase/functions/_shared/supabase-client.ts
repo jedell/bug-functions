efd38da97ceab7ebfc45d04a071e23f0dbaf816e
joe <jre2016@gmail.com>
2023-10-12 22:51:33 -0400

    dotenv

--- supabase/functions/_shared/supabase-client.ts
+++ supabase/functions/_shared/supabase-client.ts
@@ -1,9 +1,10 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.26.0";
-import { config } from "https://deno.land/x/dotenv/mod.ts";
+import { load } from "https://deno.land/std@0.204.0/dotenv/mod.ts";
 
+await load({export: true, envPath: "./.env.test"});
 
-const supabaseUrl = Deno.env.get("MY_SUPABASE_URL")
-const supabaseKey = Deno.env.get("MY_SUPABASE_KEY");
+export const supabaseUrl = Deno.env.get("MY_SUPABASE_URL")
+export const supabaseKey = Deno.env.get("MY_SUPABASE_KEY");
 // const supabaseUrl = config({ path: ".env" }).MY_SUPABASE_URL;
 // const supabaseKey = config({ path: ".env" }).MY_SUPABASE_KEY;
 console.log(supabaseUrl)
