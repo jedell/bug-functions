d33b983d5f3bc41a1668217910eeaaf99c64fafc
joe <jre2016@gmail.com>
2023-10-22 15:37:23 -0400

    add prompts

--- supabase/functions/_shared/config/prompts.ts
+++ supabase/functions/_shared/config/prompts.ts
@@ -21,3 +21,11 @@ const generateEventsPrompt = `Using the following information about the user's p
 const finalEventPrompt = "Using the following list of events that the user enjoys, can you create a detailed description of a new activity that the user would enjoy?";
 
 const friendGeneratePrompt = 'Based on the events above, generate more events that both the user and their friend would enjoy doing together'
+
+export {
+    activityListTemplate,
+    finalActivityTemplate,
+    generateEventsPrompt,
+    finalEventPrompt,
+    friendGeneratePrompt
+}
